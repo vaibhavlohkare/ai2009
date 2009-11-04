4892327aec7e3a41e26f7cfb173580382de5c48c
icemos <icemos@74b4a128-c4d1-11de-ba9a-9db95b2bc6c5>
2009-11-04 06:30:20 +0000

    the first test file

---
+++ trunk/Friends/src/helloword.java
@@ -0,0 +1,12 @@
+
+public class helloword {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Here is the ai2009 project.");
+	}
+
+}
